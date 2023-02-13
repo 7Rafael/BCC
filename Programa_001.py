43053eaa862922e187a6889b15970598b5e86b51
7Rafael <correiarafael2021@gmail.com>
2023-02-13 18:28:05 -0300

    Add files via upload

---
+++ Programa_001.py
@@ -0,0 +1,14 @@
+"""
+Teste
+"""
+#teste 
+
+questionario = bool(input("Quer ver um coelho? Responda com Y ou N"))
+coelho = """
+(\   /)
+ (* *)
+ (   )
+  w w
+"""
+
+if(questionario):print(coelho)
