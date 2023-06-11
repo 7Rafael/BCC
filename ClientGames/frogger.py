2f40f794308f71f4be66107f99492bf6440d26a4
7Rafael <correiarafael2021@gmail.com>
2023-06-11 13:37:10 -0300

    Update frogger.py

--- ClientGames/frogger.py
+++ ClientGames/frogger.py
@@ -1,21 +1,34 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
+from button import Button
+
 
 pygame.init()
 pygame.mixer.init()
 clock = pygame.time.Clock()
 
-width = 800
-height = 600
+pygame.time.wait(1000)
+#1280, 720
+width =1280 #800
+height =720 #600
 speed = 0.6
-playerSpeed = 0.6
-
+playerSpeed = 5
+win = 0
 red = (255, 0, 0)
 select_color = (60, 130, 210)
 color_hover = (255, 255, 0)
 
+BG = pygame.image.load("assets/Background.png")
+
+confirmSound = pygame.mixer.Sound('assets/8bit_kill_vo_01.ogg')
+confirmSound.set_volume(0.01)
+
+pygame.mixer.music.load("assets/Eric Skiff- Underclocked.mp3")
+pygame.mixer.music.set_volume(0)
+pygame.mixer.music.play(-1)
+
 player = pygame.image.load("assets/chicken.png")
-player = pygame.transform.scale(player, (50, 50))
+player = pygame.transform.scale(player, (62, 50))
 colliderPlayer = player.get_rect()
 
 car = pygame.image.load("assets/car.png")
@@ -23,10 +36,30 @@ car = pygame.transform.scale(car, (62, 96))
 car = pygame.transform.rotate(car, (-90))
 colliderCar = car.get_rect()
 
-car1 = pygame.image.load("assets/car.png")
-car1 = pygame.transform.scale(car1, (62, 96))
-car1 = pygame.transform.rotate(car1, (90))
-colliderCar1 = car.get_rect()
+car2 = pygame.image.load("assets/car.png")
+car2 = pygame.transform.scale(car2, (62, 96))
+car2 = pygame.transform.rotate(car2, (-90))
+colliderCar2 = car2.get_rect()
+
+car3 = pygame.image.load("assets/car.png")
+car3 = pygame.transform.scale(car3, (62, 96))
+car3 = pygame.transform.rotate(car3, (-90))
+colliderCar3 = car3.get_rect()
+
+carR = pygame.image.load("assets/car.png")
+carR = pygame.transform.scale(carR, (62, 96))
+carR = pygame.transform.rotate(carR, (90))
+colliderCarR = carR.get_rect()
+
+carR2 = pygame.image.load("assets/car.png")
+carR2 = pygame.transform.scale(carR2, (62, 96))
+carR2 = pygame.transform.rotate(carR2, (90))
+colliderCarR2 = carR2.get_rect()
+
+carR3 = pygame.image.load("assets/car.png")
+carR3 = pygame.transform.scale(carR3, (62, 96))
+carR3 = pygame.transform.rotate(carR3, (90))
+colliderCarR3 = carR3.get_rect()
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Frogger")
@@ -34,41 +67,21 @@ pygame.display.set_caption("Frogger")
 bg = pygame.image.load("assets/BackgroundFrogger.png")
 bg = pygame.transform.scale(bg, (width, height))
 
-#pygame.draw.rect(car, (0, 0, 0), colliderCar, 4)
-#pygame.draw.rect(car1, (0, 0, 0), colliderCar1, 4)
-#pygame.draw.rect(player, (255, 0, 0), colliderPlayer, 4)
-class Button:
-    def __init__(self, text, pos):
-        self.text = text
-        self.pos = pos
-        self.rect = pygame.Rect(pos[0] - 75, pos[1] - 25, 150, 50)
-        self.select = False
-
-    def draw(self):
-        if self.select:
-            pygame.draw.rect(screen, select_color, self.rect)
-        else:
-            pygame.draw.rect(screen, red, self.rect)
-
-        font = self.font = pygame.font.Font("assets/font.ttf", 24)
-        text_render = self.font.render(self.text, True, (255, 255, 255))
-        text_pos = text_render.get_rect(center=self.pos)
-        screen.blit(text_render, text_pos)
-
-button_start = Button("Start", (width // 2, height // 2 - 50))
-button_exit = Button("Exit", (width // 2, height // 2 + 50))
-pygame.display.update()
-
-def start_game(carX, carX2):
-    global colliderCar1,colliderPlayer,colliderCar
+def check_win():
+    global playerSpeed, win
+    if colliderPlayer.y == 0:
+        win += 1
+        start_game()
+def get_font(size):  # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
+def start_game():
+    global colliderPlayer,colliderCar, colliderCar2, playerSpeed, win
     Run = True
-
     width_element = 50
     height_element = 50
-    carX2 = 690
-
+    colliderPlayer.y = height
+    colliderPlayer.x = width/2
     while Run:
-
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             colliderPlayer.y -= playerSpeed
@@ -78,7 +91,6 @@ def start_game(carX, carX2):
             colliderPlayer.x -= playerSpeed
         if keys[pygame.K_RIGHT]:
             colliderPlayer.x += playerSpeed
-
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -86,80 +98,97 @@ def start_game(carX, carX2):
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 pygame.quit()
                 exit()
-
-        screen.fill((0, 0, 0))
         screen.blit(bg, (0, 0))
-
         # Limitar a posição horizontal do avatar
-
         if colliderPlayer.x < 0:
             colliderPlayer.x = 0
         elif colliderPlayer.x > width - width_element:
             colliderPlayer.x = width - width_element
-
         # Limitar a posição vertical do avatar
         if colliderPlayer.y < 0:
-            colliderPlayer.y= 0
+            colliderPlayer.y = 0
         elif colliderPlayer.y > height - height_element:
             colliderPlayer.y = height - height_element
-
         screen.blit(player, colliderPlayer)
-        screen.blit(car, (colliderCar.x , 60))
-        colliderCar.x += speed
+        screen.blit(car, colliderCar)
+        screen.blit(car2, colliderCar2)
+        screen.blit(car3, colliderCar3)
+        screen.blit(carR, colliderCarR)
+        screen.blit(carR2, colliderCarR2)
+        screen.blit(carR3, colliderCarR3)
 
-        carz = screen.blit(car1, colliderCar1)
-        colliderCar1.x -= speed
-        #screen.blit(car, (carX, 400))
-        #screen.blit(car, (carX, 475))
-        #screen.blit(car, (carX, 330))
-        #screen.blit(car1, (carX2, 210))
-        #screen.blit(car1, (carX2, 135))
-        #screen.blit(car1, (carX2, 60))
-        if colliderPlayer.colliderect(colliderCar):
-           print("Bateu")
-        if colliderCar.x == width:
+        #set values y of each car
+        colliderCarR.y = 85
+        colliderCarR2.y = 170
+        colliderCarR3.y = 255
+
+        colliderCar.y = 400
+        colliderCar2.y = 485
+        colliderCar3.y = 570
+
+        #set speed for each car
+        colliderCarR.x -= 8
+        colliderCarR3.x -= 10
+        colliderCarR2.x -= 5
+        colliderCar.x += 6
+        colliderCar2.x += 10
+        colliderCar3.x += 12
+
+        if colliderPlayer.colliderect(colliderCar) or colliderPlayer.colliderect(colliderCar2)  or colliderPlayer.colliderect(colliderCar3) or colliderPlayer.colliderect(colliderCarR) or colliderPlayer.colliderect(colliderCarR2) or colliderPlayer.colliderect(colliderCarR3):
+            win = 0
+            main()
+        if colliderCar.x >= width:
             colliderCar.x = 0
-        if colliderCar1.x == 0:
-            colliderCar1.x = 800
-
-        carX += speed
-        carX2 -= speed
-        pygame.draw.rect(screen, (0, 0, 0), colliderCar, 4)
+        if colliderCar2.x >= width:
+            colliderCar2.x = 0
+        if colliderCar3.x >= width:
+            colliderCar3.x = 0
+        if colliderCarR.x <= 0:
+            colliderCarR.x = 1280
+        if colliderCarR2.x <= 0:
+            colliderCarR2.x = 1280
+        if colliderCarR3.x <= 0:
+            colliderCarR3.x = 1280
+        win_text = get_font(20).render(f"{win}", True, "Red")
+        win_rect = win_text.get_rect(center=(20, 20))
+        screen.blit(win_text, win_rect)
+        check_win()
         pygame.display.update()
+        clock.tick(60)
 def exit_game():
     pygame.quit()
     exit()
-
 def main():
-    menuOn = True
     while True:
-        clock.tick(60)
+
+        screen.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("Frogger", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
+                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="#b68f40")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 400),
+                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="#b68f40")
+
+        screen.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(screen)
+
         for event in pygame.event.get():
-            if event.type == QUIT:
-                exit_game()
-            elif event.type == MOUSEBUTTONDOWN:
-                if menuOn:
-                    if button_start.rect.collidepoint(event.pos):
-                        menuOn = False
-                        pygame.mixer.music.stop()
-                        start_game(speed, speed)
-
-                    elif button_exit.rect.collidepoint(event.pos):
-                        exit_game()
-            if event.type == MOUSEMOTION:
-                if button_start.rect.collidepoint(event.pos):
-                    button_start.select = True
-                else:
-                    button_start.select = False
-
-                if button_exit.rect.collidepoint(event.pos):
-                    button_exit.select = True
-                else:
-                    button_exit.select = False
-        screen.fill((0, 0, 0))
-        if menuOn:
-            button_start.draw()
-            button_exit.draw()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    start_game()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
         pygame.display.update()
 main()
 pygame.quit()
